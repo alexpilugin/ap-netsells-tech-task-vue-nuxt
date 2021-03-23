d3c65f9e2f9f14b34c6ace1766763d3ce02ab5d6
Alex Pilugin <alex.pilugin@microsec.co.uk>
2021-03-23 03:22:47 +0000

    StepIndicators fixed

--- components/StepIndicators.vue
+++ components/StepIndicators.vue
@@ -8,23 +8,27 @@
         elevation="0"
         @click.stop="navigate(`/step-${index + 1}`)"
       >
-        <v-card-title class="pad-x mt-0">
-          <h2 class="text-center subtitle-1 text-uppercase mb-3 mt-0">
-            Step {{ index + 1 }}
-          </h2>
-        </v-card-title>
-        <v-card-text>
-          <v-slider
-            :v-model="step.sliderVal"
-            label=""
-            color="roman"
-            :track-color="isDarkTheme ? '#060505' : '#E3E3E3'"
-            :min="0"
-            :max="10"
-            step="1"
-            readonly
-          ></v-slider>
-        </v-card-text>
+        <v-list-item three-line>
+          <v-list-item-content>
+            <v-list-item-title class="headline mb-1">
+              <h2 class="text-center subtitle-1 text-uppercase mb-0 mt-0">
+                Step {{ index + 1 }}
+              </h2>
+            </v-list-item-title>
+            <v-list-item-subtitle>
+              <v-slider
+                :v-model="step.sliderVal"
+                label=""
+                color="roman"
+                :track-color="isDarkTheme ? '#060505' : '#E3E3E3'"
+                :min="0"
+                :max="10"
+                step="1"
+                readonly
+              ></v-slider>
+            </v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
       </v-card>
     </v-col>
   </v-row>
@@ -53,18 +57,40 @@ export default {
 .step-indicators {
   .card-indicator {
     height: 88px;
+    margin: 0 30px;
     cursor: pointer !important;
     &.m-over {
       border: solid 1px $color-brand-red !important;
     }
+    .v-list-item__content {
+      padding-left: 20px;
+      padding-right: 20px;
+    }
 
     .v-slider--horizontal {
+      width: 50%;
       .v-slider__track-container {
         height: 6px;
         .v-slider__track-background {
           border-radius: 3px;
         }
       }
+    }
+  }
+}
+.theme--dark {
+  .step-indicators {
+    .card-indicator {
+      border: 1px solid $color-grey-tint-4;
+      background: linear-gradient(-43.63deg, #363535, #3e3e3e);
+    }
+  }
+}
+.theme--light {
+  .step-indicators {
+    .card-indicator {
+      border: 1px solid $color-grey-tint-1;
+      background: linear-gradient(134.13deg, #ffffff, #fcfcfc);
     }
   }
 }
